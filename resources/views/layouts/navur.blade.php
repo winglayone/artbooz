f0db9f1b840abc68ac37013ab68295dc42054121
winglayoe <muhammadfarcry@gmail.com>
2021-02-11 05:13:35 -0800

    control projek artbooz

--- resources/views/layouts/navur.blade.php
+++ resources/views/layouts/navur.blade.php
@@ -30,7 +30,7 @@
       <div class="row">
         <div class="col-sm-12">
         <nav class="navbar navbar-expand-lg navbar-light">
-          <a class="navbar-brand" href="#"><img class="logogambar" src="{{ asset('img/Logo.png') }}"></a>
+          <a class="navbar-brand" href="/"><img class="logogambar" src="{{ asset('img/Logo.png') }}"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
